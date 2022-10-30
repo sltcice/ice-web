573e15998eadb6079eba167f3879670ed4741fe8
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-10-30 05:34:53 +0530

    new

---
+++ inc/navbar.php
@@ -0,0 +1,23 @@
+<nav>
+
+<div class="left">
+    <img src="img/logob.png" alt="ICE at SLTC Logo">
+    <h2>ICE at SLTC</h2>
+</div>
+
+<div class="right">
+    <ul>
+        <a href="#"><li>Home</li></a>
+        <a href="#"><li>News</li></a>
+        <a href="#"><li>Contact</li></a>
+        <a href="#"><li>Privecy Policy</li></a>
+        <a href="#"><li>About</li></a>
+
+    </ul>
+    <a href="#"><button>Login</button></a>
+</div>
+
+
+
+
+</nav>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// html tag / title/ head tag 
+// css link
+include "inc/head.php";
+
+
+// Nav bar include
+include "inc/navbar.php";
+
+
+
+// home page
+include "inc/home.php";
+
+
+
+
+
+
+
+
+
+
+// footer and closing body and html tags
+include "inc/footer.php";
+?>
