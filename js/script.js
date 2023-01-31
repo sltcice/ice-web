3d8a492fdc4a53ddd8d40cd65ac08f51a0c793c6
Sahan Kaushalya <mkskaushalya@gmail.com>
2023-02-01 00:09:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function scrollHeader(){
 
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 80) header.classList.add('scroll-nav');  else header.classList.remove('scroll-nav');
-    if(this.scrollY >= 80) logo.src = "img/logob.png";  else logo.src = "img/logow.png";
+    // if(this.scrollY >= 80) logo.src = "img/logob.png";  else logo.src = "img/logow.png";
 }
 window.addEventListener('scroll', scrollHeader)
 
