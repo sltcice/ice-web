31c7ee18c4ded144a675480257c63f6b80bd8160
Sahan Kaushalya <mkskaushalya@gmail.com>
2023-02-04 19:09:25 +0530

    Create navbar.php

---
+++ inc/pages/navbar.php
@@ -0,0 +1,25 @@
+<nav id="nav">
+
+<div class="left">
+    <img src="img/logow.png" alt="ICE at SLTC Logo" id="top-logo">
+    <h2>ICE at SLTC</h2>
+</div>
+
+<div class="right">
+    <ul class="navmenu">
+        <a href="?page=home"><li class="nav-link">Home</li></a>
+        <a href="?page=news"><li class="nav-link">News</li></a>
+        <a href="?page=contact"><li class="nav-link">Contact</li></a>
+        <a href="?page=privacy"><li class="nav-link">Privecy Policy</li></a>
+        <a href="?page=about"><li class="nav-link">About</li></a>
+
+    </ul>
+    <a href="?page=login"><button>Login</button></a>
+    <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+
+</div>
+</nav>
