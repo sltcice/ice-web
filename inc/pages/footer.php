b3d31fd24005d6b2bbcea9b4a268474e2b27bcc0
Sahan Kaushalya <mkskaushalya@gmail.com>
2023-02-04 19:09:00 +0530

    Create footer.php

---
+++ inc/pages/footer.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="contact">
+        
+        <div class="g-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1583.8085412557036!2d80.09246965019517!3d6.854127492374064!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae252d0dda502e5%3A0xcd9bc463bd5822de!2sSLTC%20Research%20University!5e0!3m2!1sen!2slk!4v1667135360399!5m2!1sen!2slk" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="cont-details">
+            <table>
+                <tr><td><i class="fas fa-map-marker-alt"></i></td><td>SLTC Research University<br>Ingiriya Road, Padukka, Sri Lanka.</td></tr>
+                <tr><td><i class="fas fa-phone-volume"></i></td><td> +94 11 2100 500</td></tr>
+                <tr><td><i class="fas fa-envelope"></i></td><td> info@sltc.ac.lk</td></tr>
+
+            </table>
+        </div>
+    </div>
+    <div class="feedback">
+        <form action="inc/action.php">
+            <input type="text" name="name" placeholder="Name" autocomplete="off">
+            <input type="text" name="tel" placeholder="Phone" autocomplete="off">
+            <input type="email" name="email" placeholder="Email" autocomplete="off">
+            <input type="text" name="subject" placeholder="Subject" autocomplete="off">
+            <textarea name="massege" id="" cols="30" rows="10" placeholder="Type your valueble feedback here..."></textarea>
+            <button type="Submit">Submit</button>
+            
+
+        </form>
+    </div>
+    <div class="copyright">
+     <p>&copy; ICE at SLTC ~ 2022</p>
+    </div>
+</footer>
+
+
+<script src='js/script.js'></script>
+
+</body>
+</html>
